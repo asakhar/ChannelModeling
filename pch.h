1b0bcbff9af7af4e585ab8a03e943d74d0bec9cb
asakhar <a@sakhar.ru>
2021-02-27 01:43:23 +0300

    pch

---
+++ pch.h
@@ -0,0 +1,25 @@
+#include <any>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <random>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <vector>
+#if __cplusplus > 201703L && __cpp_concepts >= 201907L
+#include <concepts>
+#endif
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <limits>
+#include <list>
+#include <memory>
+#include <random>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>

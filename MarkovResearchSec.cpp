505c14f66e4ae1aac329a8b54cd64acfc2adbec0
asakhar <a@sakhar.ru>
2021-03-06 16:22:19 +0300

    prettify

--- MarkovResearchSec.cpp
+++ MarkovResearchSec.cpp
@@ -1,10 +1,10 @@
-#include "BasicUnits/MarkovBinaryChannel.hpp"
 #include "BasicUnits/BinaryGenerator.hpp"
+#include "BasicUnits/MarkovBinaryChannel.hpp"
 #include "metainfo.hxx"
 #include <array>
 #include <fstream>
 
-int main(int /*argc*/, char const */*argv*/[]) {
+int main(int /*argc*/, char const * /*argv*/[]) {
   double const pi0 = 0;
   double const pi1 = 0.3;
   double const pi2 = 0.7;
@@ -27,18 +27,18 @@ int main(int /*argc*/, char const */*argv*/[]) {
       {p00, p01, p02}, {p10, p11, p12}, {p20, p21, p22}}};
   P.normalize(P.Rows);
   std::cout << "P_state_transition =\n";
-  for(auto row: P)
-  {
+  for (auto row : P) {
     std::cout << "[";
-    for(auto& col : row)
-      std::cout << std::setw(4) <<  col;
+    for (auto &col : row)
+      std::cout << std::setw(4) << col;
     std::cout << " ]\n";
   }
   double const prob = 0.3;
-  Model model{BinaryGenerator(prob, n), MarkovBinaryChannel(3, 0, P, {pi0, pi1, pi2})};
+  Model model{BinaryGenerator(prob, n),
+              MarkovBinaryChannel(3, 0, P, {pi0, pi1, pi2})};
   // model >> MarkovBinaryChannel(3, 0, P, {pi0, pi1, pi2});
   auto const iterations = 1000000;
-  std::array<size_t, n+1> exact_errs{0};
+  std::array<size_t, n + 1> exact_errs{0};
   // auto exact1 = 0;
   // auto exact2 = 0;
   // auto exact3 = 0;
@@ -46,9 +46,10 @@ int main(int /*argc*/, char const */*argv*/[]) {
     MetaInfo meta;
     auto res = model(meta);
     auto exact_out = meta.get<BinaryGenerator::GeneratorOut>();
-    auto zip = [](auto& v1, auto& v2) -> std::vector<std::pair<decltype(v1[0]), decltype(v2[0])>> {
+    auto zip = [](auto &v1, auto &v2)
+        -> std::vector<std::pair<decltype(v1[0]), decltype(v2[0])>> {
       std::vector<std::pair<decltype(v1[0]), decltype(v2[0])>> result;
-      for(size_t i =0; i < std::min(v1.size(), v2.size()); ++i) {
+      for (size_t i = 0; i < std::min(v1.size(), v2.size()); ++i) {
         result.emplace_back(v1[i], v2[i]);
       }
       return std::move(result);
@@ -56,7 +57,7 @@ int main(int /*argc*/, char const */*argv*/[]) {
     // auto res = model(input);
     int errs = 0;
     for (auto &pair : zip(res, exact_out.data))
-      errs += static_cast<int>(pair.first^pair.second);
+      errs += static_cast<int>(pair.first ^ pair.second);
     ++exact_errs[errs];
     // switch (errs) {
     // case 1:
@@ -73,22 +74,23 @@ int main(int /*argc*/, char const */*argv*/[]) {
     // }
   }
   std::ofstream file{"output.csv"};
-  std::cout << "total iterations: " << iterations << "\nP(m,n)  as  n=46:";
-  std::cout.precision(std::numeric_limits< double >::max_digits10);
-  file.precision(std::numeric_limits< double >::max_digits10);
+  std::cout << "total iterations: " << iterations << "\nP(m,n)  as  n=" << n
+            << ":";
+  std::cout.precision(std::numeric_limits<double>::max_digits10);
+  file.precision(std::numeric_limits<double>::max_digits10);
   file << "number_of_errors,probability\n";
-  for(size_t i = 0; auto& item : exact_errs) {
+  for (size_t i = 0; auto &item : exact_errs) {
     double probability = (double)item / iterations;
     file << i << "," << std::fixed << probability << "\n";
-    std::cout << "\n P(" << std::setw(2) << i++ << ",46) = "
-            << std::fixed << probability;
+    std::cout << "\n P(" << std::setw(2) << i++ << "," << n
+              << ") = " << std::fixed << probability;
   }
   std::cout << std::endl;
   file.close();
-            //    "\n P(1,46) = "
-            // << (double)exact1 / iterations
-            // << "\n P(2,46) = " << (double)exact2 / iterations
-            // << "\n P(3,46) = " << (double)exact3 / iterations << std::endl;
+  //    "\n P(1,46) = "
+  // << (double)exact1 / iterations
+  // << "\n P(2,46) = " << (double)exact2 / iterations
+  // << "\n P(3,46) = " << (double)exact3 / iterations << std::endl;
   // for (auto &cur : exact1_2or3) {
   //   for (auto item : cur)
   //     std::cout << item << " ";

3c61b57315d63e42912192afd48c0d88ebe8966b
asakhar <a@sakhar.ru>
2021-05-27 20:00:57 +0300

    research plots

--- test1.cpp
+++ test1.cpp
@@ -34,7 +34,9 @@ int main(int argc, char *argv[]) {
   std::string cldir = argv[1];
   std::cout << "Current CL path: \n" << cldir << std::endl;
 
-  DecoderMinSumByIndex decoder(2, 3, 100, 100, cldir, 124U); //123 is the worst seed for generating matrix
+  DecoderMinSumByIndex decoder(
+      2, 3, 100, 100, cldir,
+      124U); // 123 is the worst seed for generating matrix
   GaussianChannel channel{0.F};
   using DataType = std::vector<float>;
   using Container = struct { DataType data; };
@@ -52,36 +54,36 @@ int main(int argc, char *argv[]) {
   const auto max_ser = 30;
   const auto power_step = .005F;
   const auto max_power = .7F;
-  float noise_power = .4F;
+  for (int i = 0; i < 5; i++) {
+    float noise_power = .4F;
+    json data;
+    std::ofstream file{"result" + atos(i + 2) + ".json"};
+    MetaInfo meta;
+    meta.put(std::mt19937{i==1 ? 123U : (123U+i)});
+    while (noise_power < max_power) {
+      model.get<3>().setNoisePower(noise_power);
+      auto iters = 0UL;
+      auto ser = 0UL;
+      auto ber_cummulative = 0UL;
+      while (ber_cummulative < max_ber && ser < max_ser && iters < max_iters) {
+        iters++;
+        model(meta);
+        ber_cummulative += ber.data;
+        if (ber.data > 0)
+          ser++;
 
-  json data;
-  std::ofstream file{"result_exactly_the_same.json"};
-  MetaInfo meta;
-  meta.put(std::mt19937{123U});
-  while (noise_power < max_power) {
-    model.get<3>().setNoisePower(noise_power);
-    auto iters = 0UL;
-    auto ser = 0UL;
-    auto ber_cummulative = 0UL;
-    while (ber_cummulative < max_ber && ser < max_ser && iters < max_iters) {
-      iters++;
-      model(meta);
-      ber_cummulative += ber.data;
-      if (ber.data > 0)
-        ser++;
+        std::cout << "BER: " << ber.data << "; BERC: " << ber_cummulative
+                  << "; SER: " << ser << "; iters: " << iters << std::endl;
+      }
+      data[atos(noise_power)] = {
+          {"BERC", ber_cummulative}, {"SER", ser}, {"iters", iters}};
 
-      std::cout << "BER: " << ber.data
-                << "; BERC: " << ber_cummulative << "; SER: " << ser << "; iters: " << iters
-                << std::endl;
+      std::cout << "Number of iterations until reaching BER or CER limit: "
+                << iters << std::endl;
+
+      noise_power += power_step;
     }
-    data[atos(noise_power)] = {
-        {"BERC", ber_cummulative}, {"SER", ser}, {"iters", iters}};
-
-    std::cout << "Number of iterations until reaching BER or CER limit: "
-              << iters << std::endl;
-
-    noise_power += power_step;
+    file << data;
+    file.close();
   }
-  file << data;
-  file.close();
 }
